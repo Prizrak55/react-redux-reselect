bdc8fbd1cff2eb1b9d1b642fa5cf7180ffa1744a
Морозов Дмитрий <kor502296@gmail.com>
2023-05-03 18:15:00 +0300

    21321

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
-          <span>Learn23 </span>
+          <span>Lear </span>
           <a
             className="App-link"
             href="https://reactjs.org/"
